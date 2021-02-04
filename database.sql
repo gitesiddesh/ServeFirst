2b0325186099851708e614bd472d92d90392ca37
gitesiddesh <gitesiddesh@gmail.com>
2021-02-04 21:57:53 +0530

    database 1 commit

--- database.sql
+++ database.sql
@@ -1,6 +1,54 @@
-drop table customer;
-create table customer(reg_no serial primary key,user_name varchar(20),issue varchar(20),status varchar(20));
+drop table IF EXISTS admin;
+drop table IF EXISTS amcservice;
+drop table IF EXISTS service;
+drop table IF EXISTS corporate;
+drop table  IF EXISTS walkin;
+create table walkin(
+	username varchar(50) primary key,
+	password varchar(50),
+	fullname varchar(50),
+	email varchar(50),
+	contact bigint,
+	address varchar(150)
+	);
 
-	insert into customer values(1,'siddesh','laptop','pending');
-	insert into customer values(4,'vipul','laptop','pending');
-	insert into customer values(5,'aabhas','desktop','pending');
+	
+ create table corporate(
+	username varchar(50) primary key,
+	password varchar(50),
+	owner varchar(50),
+	company varchar(50),
+	email varchar(50),
+	contact bigint,
+	address varchar(150)
+	);
+
+create table service(
+	regno integer primary key,
+	username varchar(50),
+	usertype varchar(50),
+	helpfor varchar(50),
+	description varchar(200),
+	address varchar(150),
+	date date,
+	time time,
+	status varchar(20),
+	billamount bigint
+);
+
+create table amcservice(
+	regno integer primary key,
+	username varchar(50),
+	plan varchar(50),
+	noofmachines integer,
+	typeofmachine varchar(50),
+	address varchar(150),
+	startdate date,
+	status varchar(20),
+	billamount bigint
+);
+
+create table admin(
+	username varchar(50) primary key,
+	password varchar(50)
+);
